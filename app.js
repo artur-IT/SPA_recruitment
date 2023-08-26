1d62273ecc70429a09f7566044091ec5ab90b187
Artur Matysiak <inz.matys@gmail.com>
2023-08-26 14:16:02 +0200

    add handle curtain in gallery projects

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// ŁADUJE ZDJĘCIA DO SIATKI
+// LOAD PICTURE TO GRID
 window.onload = () => {
   const grid = document.querySelector(".our_projects .grid");
   const masonry = new Masonry(grid, {
@@ -6,3 +6,18 @@ window.onload = () => {
     gutter: 30,
   });
 };
+
+// HANDLE CURTAIN
+const expandButton = document.querySelector(".show_full_gallery");
+const fullSection = document.querySelector("section.our_projects");
+const curtain = document.querySelector(".curtain");
+let flag = true;
+
+const show = (e) => {
+  fullSection.classList.toggle("h-auto");
+  curtain.classList.toggle("curtain");
+  flag ? (e.target.textContent = "Zwiń") : (e.target.textContent = "Rozwiń");
+  flag = !flag;
+};
+
+expandButton.addEventListener("click", show);
